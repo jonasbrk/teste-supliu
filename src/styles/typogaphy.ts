ee690b67cfc92c452a3f4820eb71fe682b4022dd
João Pedro <jonas0378@gmail.com>
2022-05-12 12:45:25 -0300

    chore: typography

---
+++ src/styles/typogaphy.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+interface TypographyProps {
+    color?: string;
+    fontWeight: 'bold' | 'regular' | 'light';
+    marginTop?: string;
+    marginBottom?: number;
+    marginLeft?: number;
+    marginRight?: number;
+  }
+
+const fontWeight: Record<TypographyProps['fontWeight'], number> = {
+  bold: 700,
+  regular: 400,
+  light: 100,
+};
+
+export const Title = styled.h1<TypographyProps>`
+  font-size: 34px;
+  color: ${(props) => props.color};
+  font-weight: ${(props) => fontWeight[props.fontWeight]};
+  margin-top: ${(props) => props.marginTop && props.marginTop + 'px'};
+  margin-bottom: ${(props) => props.marginBottom && props.marginBottom  + 'px'};
+  margin-left: ${(props) => props.marginLeft && props.marginLeft + 'px'};
+  margin-right: ${(props) => props.marginRight && props.marginRight + 'px' };
+`;
+
+export const Description = styled.h3<TypographyProps>`
+  font-size: 16px;
+  color: ${(props) => props.color};
+  font-weight: ${(props) => fontWeight[props.fontWeight]};
+  margin-top: ${(props) => props.marginTop && props.marginTop + 'px'};
+  margin-bottom: ${(props) => props.marginBottom && props.marginBottom  + 'px'};
+  margin-left: ${(props) => props.marginLeft && props.marginLeft + 'px'};
+  margin-right: ${(props) => props.marginRight && props.marginRight + 'px' };
+`;
+
+export const Small = styled.h4<TypographyProps>`
+  font-size: 14px;
+  color: ${(props) => props.color};
+  font-weight: ${(props) => fontWeight[props.fontWeight]};
+  margin-top: ${(props) => props.marginTop && props.marginTop + 'px'};
+  margin-bottom: ${(props) => props.marginBottom && props.marginBottom  + 'px'};
+  margin-left: ${(props) => props.marginLeft && props.marginLeft + 'px'};
+  margin-right: ${(props) => props.marginRight && props.marginRight + 'px' };
+`;
