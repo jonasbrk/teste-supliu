1496c9bfd6305f83da4f6e937cb7e921ba11b6a2
João Pedro <jonas0378@gmail.com>
2022-05-12 12:44:48 -0300

    chore: updating colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,10 @@
-export const colors = {
+export const Colors = {
   primary: '#4a90e2',
+  darkerBlue: '#3A71B0',
   white: '#fff',
+  halfTransparent: 'rgba(255,255,255,0.8)',
+  transparent: 'transparent',
+  gray: '#919190',
+  darkerGray: '#3f3f3f',
+  danger: '#E36C62',
 }; 
