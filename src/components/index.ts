b11ccb9cf68ea3899defcfa2d246c9ac63d88a4c
João Pedro <jonas0378@gmail.com>
2022-05-16 20:34:35 -0300

    chore: export components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export {Layout} from './Layout';
+export {Button} from './Button';
+export {SearchBar} from './SearchBar';
+export {Album} from './Album';
+export {Song} from './Song';
+export {NewSong} from './NewSong';
+export {Modal} from './Modal';
