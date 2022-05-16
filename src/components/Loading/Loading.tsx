92c47b2977c2f9836c06d655f78383eaf242260b
João Pedro <jonas0378@gmail.com>
2022-05-16 20:23:44 -0300

    chore: fill prop

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,11 +4,18 @@ import { Colors } from '../../styles/colors';
 import { Small } from '../../styles/typogaphy';
 import { LoadingContainer, LoadingIconWapper } from './Loading.styles';
 
-export const Loading = () => {
+interface LoadingProps {
+  fill?: string
+}
+
+export const Loading: React.FC<LoadingProps> = (props) => {
+
+  const {fill = Colors.primary} = props;
+
   return (
     <LoadingContainer>
       <LoadingIconWapper>
-        <LoadingIcon fill={Colors.primary}/>
+        <LoadingIcon fill={fill}/>
       </LoadingIconWapper>
     </LoadingContainer>
   );
