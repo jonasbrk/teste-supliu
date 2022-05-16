6c7748fe2060d939e209cfa867cd2e90f1e7f7e1
João Pedro <jonas0378@gmail.com>
2022-05-16 20:33:07 -0300

    feat: delete track func

--- src/components/Song/Song.styles.ts
+++ src/components/Song/Song.styles.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
+import { Button } from '../Button';
 
 export const SongContainer = styled.div`
 width: 100%;
 height: 30px;
 display: grid;
 grid-gap: 16px;
-grid-template-columns: [index] 16px [first] 1fr [last] 55px;
+grid-template-columns: [index] 16px [first] 1fr [delete] auto [last] 55px;
 `;
 
 interface SongWrapperProps {
-    column: 'index' |  'first' | 'last'
+    column: 'index' |  'first' | 'delete' | 'last'
 }
 
 export const SongWrapper = styled.div<SongWrapperProps>`
@@ -17,4 +19,22 @@ height: 100%;
 display: flex;
 align-items: center;
 grid-column: ${props => props.column && props.column};
+`;
+
+export const SongDeleteButton = styled.div`
+
+height: 16px;
+width: 16px;
+display: flex;
+justify-content: center;
+align-items: center;
+
+opacity: 0;
+transition: all 0.1s ease-in-out;
+
+${SongContainer}:hover & {
+    opacity: 1;
+
+}
+
 `;
--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TrashIcon } from '../../assets/svg';
+import { Api } from '../../services/api';
+import { Colors } from '../../styles/colors';
 import { Small } from '../../styles/typogaphy';
 import { ITrack } from '../../typings/response';
+import { AlbumsContext } from '../../utils/context';
 import { useTimeFormater } from '../../utils/useTimeFormater';
-import { SongContainer, SongWrapper } from './Song.styles';
+import { Warning } from '../Warning';
+import { dataWarningProps } from '../Warning/Warning';
+import { SongContainer, SongWrapper, SongDeleteButton } from './Song.styles';
 
 interface SongProps {
 data: ITrack
+disable: boolean
 }
 export const Song: React.FC<SongProps> = (props) => {
-  const {id, duration, number, title } = props.data;
+  const { data, disable } = props;
+  const {id, album, duration, number, title } = data;
+  const {albumsData, setAlbumsData} = useContext(AlbumsContext);
+  const [warning, setWarning] = useState<boolean>(false);
+  const [ loading, setLoading] = useState<boolean>(false);
+
+  const handleDelete = async (dataProps : ITrack, validate: boolean ) => {
+
+    if(!validate){
+      setWarning(true);
+    }
+    else{
+      setLoading(true);
+      try {
+        const response = await Api('/track/' + dataProps.id,{
+          method:'DELETE',
+        });
+        if(response.status == 200){
+          
+          const newData = albumsData.filter((e) => e.id != album.id);
+          const newAlbuns = [...newData, 
+            {...album,
+              tracks: album.tracks ?
+                album.tracks.filter((e)=> e.id != id) : [],
+            }];
+
+          setAlbumsData(newAlbuns);
+          
+        }else{
+          console.log(response);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      
+    }
+  
+  };
+
+  const navigate = useNavigate();
   return (
     <SongContainer>
+      <Warning 
+        data={{...data, type: 'track' , name: title}}
+        isOpen={warning}
+        onClose={()=> setWarning(false)}
+        onValidate={()=> handleDelete(data, true)}
+        loading={loading}
+      ></Warning>
       <SongWrapper column='index'>
         <Small fontWeight='regular'>
           {number}
@@ -21,6 +75,13 @@ export const Song: React.FC<SongProps> = (props) => {
           {title}
         </Small>      
       </SongWrapper>
+      {!disable &&
+      <SongWrapper column='delete'>
+        <SongDeleteButton onClick={() => handleDelete(data, false)}>
+          <TrashIcon fill={Colors.danger}/>
+        </SongDeleteButton>
+      </SongWrapper>
+      }
       <SongWrapper column='last'>
         <Small fontWeight='regular'>
           {useTimeFormater(duration)}
