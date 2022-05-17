45fb651b762daacd3c139f83870cd7b6bbe3e541
João Pedro <jonas0378@gmail.com>
2022-05-17 17:45:43 -0300

    chore: refactoring code

--- src/utils/useTimeFormater.ts
+++ src/utils/useTimeFormater.ts
@@ -4,16 +4,16 @@ export const useTimeFormater = (seg : number, cursive ?: boolean) => {
   let formatedTime = '';
   
   if (seg <= 3600) {
-    let minString = Math.floor(seg / 60);
-    let segString = String(((seg % 60)).toFixed(0)).padStart(2, '0');
+    const minString = Math.floor(seg / 60);
+    const segString = String(((seg % 60)).toFixed(0)).padStart(2, '0');
 
     formatedTime = `${minString}${cursive ? 'min ' : ':'}${segString}${
       cursive ? 'seg' : ''
     }`;
   } else {
-    let hrString = Math.floor(seg / 3600);
-    let minString = Math.floor((seg % 3600) / 60);
-    let segString = Math.floor((seg % 3600) % 60);
+    const hrString = Math.floor(seg / 3600);
+    const minString = Math.floor((seg % 3600) / 60);
+    const segString = Math.floor((seg % 3600) % 60);
   
     formatedTime = `${hrString}${cursive ? 'h ' : ':'}${minString}${cursive ? 'min ' : ':'}${segString}${cursive ? 'seg' : ''}`;
   }
